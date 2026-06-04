774d369845b970a3f74ffaead51d739792c38512
Bao Anh <baoanhtrangia2005@gmail.com>
2026-06-04 14:01:14 +0700

    Change axios base URL to ngrok endpoint

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:8000", // Port của Backend
+    baseURL: "https://avenge-task-common.ngrok-free.dev", // Port của Backend
     headers: {
         'Content-Type': 'application/json',
         "ngrok-skip-browser-warning": "true",
